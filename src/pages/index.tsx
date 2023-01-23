ed432e2cb0ae96654333ef1ec31d7bb30173620a
M Naufal Badruttamam <92671053+Caknoooo@users.noreply.github.com>
2023-01-23 17:02:03 +0700

    test: testing deployment

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,16 @@
 export default function Home() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <div className="p-5">
+      <div>
+        <h2>Mobiles</h2>
+        <div className="py-4">
+          <div className="w-64">
+            <div className="bg-blue-100 dark:bg-blue-400 p-5 rounded-xl">
+              <img src="/products/iphone.png" alt="" />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
