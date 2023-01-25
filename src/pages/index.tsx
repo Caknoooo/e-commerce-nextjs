6e50eb7443e5ffca3edf4dc753465b93573b7a3a
M Naufal Badruttamam <92671053+Caknoooo@users.noreply.github.com>
2023-01-25 09:49:59 +0700

    feat: displays data from mongodb and configure it

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,45 +1,59 @@
 import { useEffect, useState } from "react";
+import Product from "components/Products";
+import { initMongoose } from "lib/mongoose";
+import { findAllProducts } from "./api/products";
 
-export default function Home() {
-  const [productsInfo, setProductsInfo] = useState([]);
+export default function Home({ products}) {
+  const [phrase, setPhrase] = useState("");
 
-  useEffect(() => {
-    fetch('/api/products').then(response => response.json()).then(json => setProductsInfo(json));
-  }, []);
+  const categoriesNames = [...new Set(products.map((p) => p.category))];
+  // console.log({ categoriesNames });
 
-  const categoriesNames = [...new Set(productsInfo.map(p => p.category))];
-  
+  // Find items
+  if (phrase) {
+    products = products.filter((p) => p.name.toLowerCase().includes(phrase));
+  }
+
   return (
     <div className="p-5">
+      <input
+        type="text"
+        value={phrase}
+        onChange={(e) => setPhrase(e.target.value)}
+        placeholder="Search for products..."
+        className="bg-gray-100 dark:bg-gray-200 w-full py-2 px-4 rounded-xl"
+      />
       <div>
-        {categoriesNames.map(categoryName => (
+        {categoriesNames.map((categoryName) => (
           <div key={categoryName}>
-            <h2 className="text-2xl">{categoryName}</h2>
+            {products.find((p) => p.category === categoryName) && (
+              <div>
+                <h2 className="text-2xl py-5 font-bold">{categoryName}</h2>
+                <div className="flex -mx-5 overflow-x-scroll snap-x scrollbar-hide">
+                  {products
+                    .filter((p) => p.category === categoryName)
+                    .map((productsInfo) => (
+                      <div key={productsInfo._id} className="px-5 snap-start">
+                        <Product {...productsInfo} />
+                      </div>
+                    ))}
+                </div>
+              </div>
+            )}
           </div>
         ))}
-        <div className="py-4">
-          <div className="w-64">
-            <div className="bg-blue-100 dark:bg-blue-400 p-5 rounded-xl">
-              <img src="/products/iphone.png" alt="" />
-            </div>
-            <div className="mt-2">
-              <h3 className="font-bold text-lg">Iphone 14 Pro</h3>
-            </div>
-            <p className="text-sm mt-2">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam
-              nemo corrupti iusto enim eligendi neque magnam dolore nam sequi
-              aliquam illo explicabo praesentium consequuntur alias cupiditate,
-              delectus eveniet? Voluptates, placeat?
-            </p>
-            <div className="flex mt-2">
-              <div className="text-2xl font-bold grow">$999</div>
-              <button className="bg-emerald-400 text-white py-1 px-3 rounded-xl">
-                +
-              </button>
-            </div>
-          </div>
-        </div>
       </div>
     </div>
   );
+}
+
+export async function getServerSideProps() {
+  await initMongoose();
+  const products = await findAllProducts();
+
+  return {
+    props: {
+      products: JSON.parse(JSON.stringify(products)),
+    },
+  };
 }
